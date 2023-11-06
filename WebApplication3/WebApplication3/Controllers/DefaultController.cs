69b632c4aaf1a3c4ddfdc9625a55b316eff2f069
Mislav Mores <mislav.mores1@gmail.com>
2023-11-06 18:11:00 +0100

    exercises1

---
+++ WebApplication3/WebApplication3/Controllers/DefaultController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace WebApplication3.Controllers
+{
+    public class DefaultController : ApiController
+    {
+        // GET: Default
+      
+
+            public string[] Get()
+            {
+                return new string[]
+                {
+                    "Hello",
+                    "World"
+                };
+            }
+          
+        
+    }
+}
